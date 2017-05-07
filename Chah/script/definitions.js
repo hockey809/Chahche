5079505b440d57b2ba6cfcf0c6ea9b382cb1ba64
Icaka <ici_ioana@abv.bg>
2017-05-07 20:37:24 +0300

    Added evaluate.js

--- Chah/script/definitions.js
+++ Chah/script/definitions.js
@@ -77,11 +77,20 @@ var Sq120ToSq64 = new Array(BRD_SQ_NUM);
 var Sq64ToSq120 = new Array(64);
 
 function RAND_32() {
-
 	return (Math.floor((Math.random()*255)+1) << 23) | (Math.floor((Math.random()*255)+1) << 16)
 		 | (Math.floor((Math.random()*255)+1) << 8) | Math.floor((Math.random()*255)+1);
-
 }
+
+var Mirror64 = [
+56	,	57	,	58	,	59	,	60	,	61	,	62	,	63	,
+48	,	49	,	50	,	51	,	52	,	53	,	54	,	55	,
+40	,	41	,	42	,	43	,	44	,	45	,	46	,	47	,
+32	,	33	,	34	,	35	,	36	,	37	,	38	,	39	,
+24	,	25	,	26	,	27	,	28	,	29	,	30	,	31	,
+16	,	17	,	18	,	19	,	20	,	21	,	22	,	23	,
+8	,	9	,	10	,	11	,	12	,	13	,	14	,	15	,
+0	,	1	,	2	,	3	,	4	,	5	,	6	,	7
+];
 
 function SQ64(sq120) { 
 	return Sq120ToSq64[(sq120)];
@@ -93,6 +102,10 @@ function SQ120(sq64) {
 
 function PCEINDEX(pce, pceNum) {
 	return (pce * 10 + pceNum);
+}
+
+function MIRROR64(sq) {
+	return Mirror64[sq];
 }
 
 var Kings = [PIECES.wK, PIECES.bK];
