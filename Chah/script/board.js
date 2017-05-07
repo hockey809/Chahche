7fab5fbf148bc20cba251e94630578a56580fc45
Icaka <ici_ioana@abv.bg>
2017-05-07 12:03:32 +0300

    Adding more qualities for the pieces

--- Chah/script/board.js
+++ Chah/script/board.js
@@ -1,9 +1,17 @@
+function PCEINDEX(pce, pceNum) {
+	return (pce * 10 + pceNum);
+}
+
 var GameBoard = {};
 
 GameBoard.pieces = new Array(BRD_SQ_NUM);
-GameBoard.side = COLOUR.WHITE;
+GameBoard.side = COLOURS.WHITE;
 GameBoard.fiftyMove = 0;
 GameBoard.hisPly = 0;
 GameBoard.ply = 0;
+GameBoard.enPas = 0; //look up the En passant move
 GameBoard.castlePerm = 0; //castling permission
-
+GameBoard.material = new Array(2); // WHITE,BLACK material of pieces
+GameBoard.pceNum = new Array(13); // indexed by Pce
+GameBoard.pList = new Array(14 * 10);
+GameBoard.posKey = 0;
