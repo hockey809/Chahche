9c783edc063bb0bc13ed8341543777a376e3e7d2
Icaka <ici_ioana@abv.bg>
2017-05-07 19:08:56 +0300

    Add/MovePiece functions

--- Chah/script/makemove.js
+++ Chah/script/makemove.js
@@ -21,3 +21,36 @@ function ClearPiece(sq) {
 	GameBoard.pList[PCEINDEX(pce, t_pceNum)] = GameBoard.pList[PCEINDEX(pce, GameBoard.pceNum[pce])];	
 
 }
+
+function AddPiece(sq, pce) {
+
+	var col = PieceCol[pce];
+	
+	HASH_PCE(pce, sq);
+	
+	GameBoard.pieces[sq] = pce;
+	GameBoard.material[col] += PieceVal[pce];
+	GameBoard.pList[PCEINDEX(pce, GameBoard.pceNum[pce])] = sq;
+	GameBoard.pceNum[pce]++;
+
+}
+
+function MovePiece(from, to) {
+	
+	var index = 0;
+	var pce = GameBoard.pieces[from];
+	
+	HASH_PCE(pce, from);
+	GameBoard.pieces[from] = PIECES.EMPTY;
+	
+	HASH_PCE(pce,to);
+	GameBoard.pieces[to] = pce;
+	
+	for(index = 0; index < GameBoard.pceNum[pce]; ++index) {
+		if(GameBoard.pList[PCEINDEX(pce,index)] == FROM) {
+			GameBoard.pList[PCEINDEX(pce,index)] = to;
+			break;
+		}
+	}
+	
+}
