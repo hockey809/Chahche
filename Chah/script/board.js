0626cbbc730d370a901065d2a7a289a8ffbf7af7
Icaka <ici_ioana@abv.bg>
2017-05-07 16:27:39 +0300

    Function for updating the piece list

--- Chah/script/board.js
+++ Chah/script/board.js
@@ -79,16 +79,9 @@ function GeneratePosKey() {
 	return finalKey;
 }
 
-function ResetBoard() {
-	var index = 0;
+function UpdateListsMaterial() {	
 	
-	for(index = 0; index < BRD_SQ_NUM; ++index) {
-		GameBoard.pieces[index] = SQUARES.OFFBOARD;
-	}
-	
-	for(index = 0; index < 64; ++index) {
-		GameBoard.pieces[SQ120(index)] = PIECES.EMPTY;
-	}
+	var piece,sq,index,colour;
 	
 	for(index = 0; index < 14 * 120; ++index) {
 		GameBoard.pList[index] = PIECES.EMPTY;
@@ -96,6 +89,32 @@ function ResetBoard() {
 	
 	for(index = 0; index < 2; ++index) {		
 		GameBoard.material[index] = 0;		
+	}	
+	
+	for(index = 0; index < 13; ++index) {
+		GameBoard.pceNum[index] = 0;
+	}
+	
+	for(index = 0; index < 64; ++index) {
+		sq = SQ120(index);
+		piece = GameBoard.pieces[sq];
+		if(piece != PIECES.EMPTY) {
+			console.log('piece ' + piece + ' on ' + sq);
+			colour = PieceCol[piece];		
+			
+			GameBoard.material[colour] += PieceVal[piece];
+			
+			GameBoard.pList[PCEINDEX(piece,GameBoard.pceNum[piece])] = sq;
+			GameBoard.pceNum[piece]++;			
+		}
+	}
+}
+
+function ResetBoard() {
+	var index = 0;
+	
+	for(index = 0; index < BRD_SQ_NUM; ++index) {
+		GameBoard.pieces[index] = SQUARES.OFFBOARD;
 	}	
 	
 	for(index = 0; index < 13; ++index) {
@@ -198,4 +217,5 @@ function ParseFen(fen) {
     }
 	
 	GameBoard.posKey = GeneratePosKey();
+	UpdateListsMaterial();
 }
