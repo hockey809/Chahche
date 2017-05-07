6de3a5f9fbe9a57f4605ea77b8756ff8c5958301
Icaka <ici_ioana@abv.bg>
2017-05-07 22:10:22 +0300

    Now we have pieces on the board

--- Chah/script/gui.js
+++ Chah/script/gui.js
@@ -1,7 +1,45 @@
 function generateFEN(){
 	console.log("button clicked\n");;
 	var fenStr = $("#fenIn").val();	
-	ParseFen(START_FEN);
+	NewGame(fenStr);
+}
+
+function NewGame(fenStr) {
+	ParseFen(fenStr);
 	PrintBoard();
-	SearchPosition();
+	SetInitialBoardPieces();
+}
+
+function ClearAllPieces() {
+	$(".Piece").remove();
+}
+
+function SetInitialBoardPieces() {
+
+	var sq;
+	var sq120;
+	var file,rank;
+	var rankName;
+	var fileName;
+	var imageString;
+	var pieceFileName;
+	var pce;
+	
+	ClearAllPieces();
+	
+	for(sq = 0; sq < 64; ++sq) {
+		sq120 = SQ120(sq);
+		pce = GameBoard.pieces[sq120];
+		file = FilesBrd[sq120];
+		rank = RanksBrd[sq120];
+		
+		if(pce >= PIECES.wP && pce <= PIECES.bK) {
+			rankName = "rank" + (rank+1);
+			fileName = "file" + (file+1);
+			pieceFileName = "images/" + SideChar[PieceCol[pce]] + PceChar[pce].toUpperCase() + ".png";
+			imageString = "<image src=\"" + pieceFileName + "\" class=\"Piece " + rankName + " " + fileName + "\"/>";
+			$("#Board").append(imageString);
+		}
+	}
+
 }
