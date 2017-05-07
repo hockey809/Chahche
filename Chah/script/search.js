7ce1f33d8562acd34025814bf8966971ce9ad45b
Icaka <ici_ioana@abv.bg>
2017-05-07 20:31:24 +0300

    Implementing Search #2

--- Chah/script/search.js
+++ Chah/script/search.js
@@ -10,17 +10,39 @@ SearchController.stop;
 SearchController.best;
 SearchController.thinking;
 
+function CheckUp() {
+	if (( $.now() - SearchController.start ) > SearchController.time) {
+		SearchController.stop == BOOL.TRUE;
+	}
+}
+
+function IsRepetition() {
+	var index = 0;
+	
+	for(index = GameBoard.hisPly - GameBoard.fiftyMove; index < GameBoard.hisPly - 1; ++index) {
+		if(GameBoard.posKey == GameBoard.history[index].posKey) {
+			return BOOL.TRUE;
+		}
+	}
+	
+	return BOOL.FALSE;
+}
+
 function AlphaBeta(alpha, beta, depth) {
 
 	if(depth <= 0) {
 		/* return Evaluate() */
 	}
 	
-	/* Check Time Up */
+	if ((SearchController.nodes & 2047) == 0) {
+		CheckUp();
+	}
 	
 	SearchController.nodes++;
 	
-	/* Check Rep() Fifty Move Rule */ 
+	if( (IsRepetition() || GameBoard.fiftyMove >= 100) && GameBoard.ply != 0) {
+		return 0;
+	}
 	
 	if(GameBoard.ply > MAXDEPTH -1) {
 		/* return Evaluate() */
@@ -29,6 +51,9 @@ function AlphaBeta(alpha, beta, depth) {
 	var Score = -INFINITE;
 	
 	GenerateMoves();
+	
+	PrintMoveList();
+
 	
 	var MoveNum = 0;
 	var Legal = 0;
@@ -42,9 +67,10 @@ function AlphaBeta(alpha, beta, depth) {
 	for(MoveNum = GameBoard.moveListStart[GameBoard.ply]; MoveNum < GameBoard.moveListStart[GameBoard.ply + 1]; ++MoveNum) {
 	
 		/* Pick Next Best Move */
-	
+		
 		Move = GameBoard.moveList[MoveNum];	
-		if(MakeMove(move) == BOOL.FALSE) {
+
+		if(MakeMove(Move) == BOOL.FALSE) {
 			continue;
 		}		
 		Legal++;
@@ -87,7 +113,7 @@ function SearchPosition() {
 	var bestScore = -INFINITE;
 	var currentDepth = 0;
 	
-	for( currentDepth = 1; currentDepth <= SearchController.depth; ++currentDepth) {
+	for( currentDepth = 1; currentDepth <= /*SearchController.depth*/ 1; ++currentDepth) {
 		
 		/* AB */
 		
